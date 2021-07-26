83bd225bb60838ad09ca70dbd47d437770db865f
Kwame <kwameagyenimboateng64@gmail.com>
2021-07-26 08:39:55 +0000

    add countdown FC to app

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Countdown from './countdown'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Countdown />
     </View>
   );
 }
@@ -15,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems:'center'
   },
 });
